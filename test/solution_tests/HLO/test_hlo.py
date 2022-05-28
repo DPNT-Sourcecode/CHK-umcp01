31db3085eea75b5181866b9f331cb44711c11e46
sbx_user1051 <sbx_user1051@169.254.68.201>
2022-05-28 10:43:51 +0000

    Sat May 28 10:43:51 UTC 2022

--- test/solution_tests/HLO/test_hlo.py
+++ test/solution_tests/HLO/test_hlo.py
@@ -1,10 +1,19 @@
 import pytest
 from solutions.HLO import hello_solution
 
+
 def test_invalid_input_type():
     with pytest.raises(ValueError):
         hello_solution.hello(1)
 
+
 def test_empty_input():
     with pytest.raises(ValueError):
         hello_solution.hello("")
+
+
+def test_correct_output(capfd):
+    hello_solution.hello("William")
+    out, err = capfd.readouterr()
+    assert out == "Hello William!\n"
+
