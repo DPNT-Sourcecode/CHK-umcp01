07b1baae528b1251f423d121530d41eb849921bd
sbx_user1051 <sbx_user1051@169.254.68.201>
2022-05-28 10:34:51 +0000

    Sat May 28 10:34:51 UTC 2022

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -1,4 +1,9 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
-def hello(friend_name):
-    raise NotImplementedError()
+def hello(friend_name: str) -> None:
+
+    if not isinstance(friend_name, str):
+        raise ValueError("friend_name must be a String")
+
+    print(f"Hello {friend_name}!")
+
