7b9128ea8b04ce1a96df42e30446f7cedcdcf929
sbx_user1051 <sbx_user1051@169.254.68.201>
2022-05-28 10:45:45 +0000

    Sat May 28 10:45:45 UTC 2022

--- test/solution_tests/HLO/test_hlo.py
+++ test/solution_tests/HLO/test_hlo.py
@@ -1,19 +1,33 @@
+"""
+    Contains tests for solutions.HLO module
+"""
 import pytest
 from solutions.HLO import hello_solution
 
 
 def test_invalid_input_type():
+    """
+    tests that invalid input types raise an error
+    """
     with pytest.raises(ValueError):
         hello_solution.hello(1)
 
 
 def test_empty_input():
+    """
+    tests that empty string input raises an error
+    """
     with pytest.raises(ValueError):
         hello_solution.hello("")
 
 
 def test_correct_output(capfd):
+    """
+    tests that the printed output is correct
+    """
     hello_solution.hello("William")
     out, err = capfd.readouterr()
     assert out == "Hello William!\n"
+    assert err == ""
+
 
