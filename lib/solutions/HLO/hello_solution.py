dcaf594b13b0f1f65c62ddff845fb92c51a66e90
sbx_user1051 <sbx_user1051@169.254.68.201>
2022-05-28 10:56:19 +0000

    Sat May 28 10:56:19 UTC 2022

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -1,20 +1,22 @@
 """
     Contains implementation of a greeting function.
 """
+
 # noinspection PyUnusedLocal
 # friend_name = unicode string
-# pylint: disable=unused-argument
-def hello(friend_name) -> str:
+def hello(friend_name: str) -> str:
     """
-    Returns a greeting in the format "Hello World!"
+    Prints a greeting in the format "Hello {friend_name}!"
 
-    :param friend_name: ignored!
-    :type friend_name: any
-    :return: A string greeting, "Hellow World!"
-    :rtype: str
+    :param friend_name: the name to use in the greeting
+    :type friend_name: str
+    :raises ValueError: If friend_name is not a valid String.
     """
-    return "Hello, World!"
 
+    if not isinstance(friend_name, str):
+        raise ValueError("friend_name must be a String.")
 
+    if not friend_name:
+        raise ValueError("friend_name must not be empty.")
 
-
+    return f"Hello, {friend_name}!"
--- test/solution_tests/HLO/test_hlo.py
+++ test/solution_tests/HLO/test_hlo.py
@@ -1,23 +1,24 @@
 """
     Contains tests for solutions.HLO module
 """
-# import pytest
+import pytest
 from solutions.HLO import hello_solution
 
-# def test_invalid_input_type():
-#     """
-#     tests that invalid input types raise an error
-#     """
-#     with pytest.raises(ValueError):
-#         hello_solution.hello(1)
+
+def test_invalid_input_type():
+    """
+    tests that invalid input types raise an error
+    """
+    with pytest.raises(ValueError):
+        hello_solution.hello(1)
 
 
-# def test_empty_input():
-#     """
-#     tests that empty string input raises an error
-#     """
-#     with pytest.raises(ValueError):
-#         hello_solution.hello("")
+def test_empty_input():
+    """
+    tests that empty string input raises an error
+    """
+    with pytest.raises(ValueError):
+        hello_solution.hello("")
 
 
 def test_correct_output():
@@ -25,9 +26,5 @@ def test_correct_output():
     tests that the printed output is correct
     """
     out = hello_solution.hello("William")
-    assert out == "Hello, World!"
 
-
-
-
-
+    assert out == "Hello, William!"
