73d990a02d9a24a95d7b8e9029c90b5e9d452a9d
sbx_user1051 <sbx_user1051@169.254.68.201>
2022-05-28 13:43:51 +0000

    Sat May 28 13:43:51 UTC 2022

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -6,63 +6,36 @@ import pytest
 from solutions.CHK.checkout_solution import checkout
 
 
-+------+-------+------------------------+
-|     | 3A for 130, 5A for 200 |
-|     | 2B for 45              |
-|     |                        |
-|     |                        |
-|     | 2E get one B free      |
-|     | 2F get one F free      |
-|     |                        |
-|     | 5H for 45, 10H for 80  |
-|     |                        |
-|     |                        |
-|     | 2K for 150             |
-|     |                        |
-|     |                        |
-|     | 3N get one M free      |
-|     |                        |
-|     | 5P for 200             |
-|     | 3Q for 80              |
-|     | 3R get one Q free      |
-|     |                        |
-|     |                        |
-|     | 3U get one U free      |
-|     | 2V for 90, 3V for 130  |
-|     |                        |
-|     |                        |
-|     |                        |
-|     |                        |
-+------+-------+------------------------+
-
-
 @pytest.mark.parametrize(
-    "skus,expected", [("A",50),
-("B",30),
-("C",20),
-("D",15),
-("E",40),
-("F",10),
-("G",20),
-("H",10),
-("I",35),
-("J",60),
-("K",80),
-("L",90),
-("M",15),
-("N",40),
-("O",10),
-("P",50),
-("Q",30),
-("R",50),
-("S",30),
-("T",20),
-("U",40),
-("V",50),
-("W",20),
-("X",90),
-("Y",10),
-("Z",50),]
+    "skus,expected",
+    [
+        ("A", 50),
+        ("B", 30),
+        ("C", 20),
+        ("D", 15),
+        ("E", 40),
+        ("F", 10),
+        ("G", 20),
+        ("H", 10),
+        ("I", 35),
+        ("J", 60),
+        ("K", 80),
+        ("L", 90),
+        ("M", 15),
+        ("N", 40),
+        ("O", 10),
+        ("P", 50),
+        ("Q", 30),
+        ("R", 50),
+        ("S", 30),
+        ("T", 20),
+        ("U", 40),
+        ("V", 50),
+        ("W", 20),
+        ("X", 90),
+        ("Y", 10),
+        ("Z", 50),
+    ],
 )
 def test_prices(skus, expected):
     """
@@ -87,6 +60,17 @@ def test_prices(skus, expected):
         ("FFF", 20),
         ("FFFF", 30),
         ("FFFFF", 40),
+        ("HHHHH", 45),
+        ("HHHHHHHHHH", 80),
+        ("KK", 150),
+        ("NNM", 120),
+        ("PPPPP", 200),
+        ("QQQ", 80),
+        ("RRRQ", 150),
+        ("UUU", 120),
+        ("UUUU", 120),
+        ("VV", 90),
+        ("VVV", 130),
     ],
 )
 def test_offers(skus, expected):
@@ -108,4 +92,5 @@ def test_invalid_input_type():
     Tests that incorrect input types return -1
     """
     assert checkout(1) == -1
+
 
