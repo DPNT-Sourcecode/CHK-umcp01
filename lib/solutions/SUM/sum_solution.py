f3b0b0d8ea628b53ef4f966199e2810454a93b1a
sbx_user1051 <sbx_user1051@169.254.68.201>
2022-05-28 10:00:27 +0000

    Sat May 28 10:00:27 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,6 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
-def compute(x, y):
+def compute(number_1: int, number_2: int) -> int:
+    if number_1 > 100 or number_1 < 0:
+        raise ValueError("{} must be greater than 0 and less than 100." % number_1)
     raise NotImplementedError()
+
