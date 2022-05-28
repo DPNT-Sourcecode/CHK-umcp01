136b79774fc7c4f67151c18248ab30ba7c5b38b1
sbx_user1051 <sbx_user1051@169.254.68.201>
2022-05-28 10:03:27 +0000

    Sat May 28 10:03:27 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,6 +1,23 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(number_1: int, number_2: int) -> int:
+    """
+    Computes the sum of two positive integers
+    Both numbers must be between 0-100.
+
+    :param number_1: The first number to sum.
+    :type number_1: int
+    :param number_2:The second number to sum.
+    :type number_2: int
+    :raises ValueError: If either number is negative or greater than 100.
+    :return: The sum of the two numbers.
+    :rtype: int
+    """
     if number_1 > 100 or number_1 < 0:
-        raise ValueError("{} must be greater than 0 and less than 100." % number_1)
-    raise NotImplementedError()
+        raise ValueError(f"{number_1} must be greater than 0 and less than 100.")
+
+    if number_2 > 100 or number_2 < 0:
+        raise ValueError(f"{number_1} must be greater than 0 and less than 100.")
+
+    return number_1 + number_2
+
 
