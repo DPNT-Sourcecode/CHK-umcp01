092adcebc605ff5104fb45bab16c635e2f2d46d5
sbx_user1051 <sbx_user1051@169.254.68.201>
2022-05-28 11:31:08 +0000

    Sat May 28 11:31:08 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,15 +3,8 @@
 
 from collections import defaultdict
 
-VALID_INPUT = (
-    "A",
-    "B",
-    "C",
-    "D"
-)
-
 COSTS = {
-    "A": {"deal":{"count": 3, "value": 130}, "cost": 50},
+    "A": {"deal": {"count": 3, "value": 130}, "cost": 50},
     "B": {"deal": {"count": 2, "value": 45}, "cost": 30},
     "C": {"cost": 20},
     "D": {"cost": 15}
@@ -24,17 +17,19 @@ def checkout(skus: str) -> int:
     code_counts = defaultdict(int)
 
     for code in skus:
-        if not code in VALID_INPUT:
+        if not code in COSTS.keys():
             return -1
         code_counts[code] += 1
 
     total_cost = 0
     for code, count in code_counts.items():
-
+        deal_cost = COSTS[code] /
+        total_cost +=
 
 
 
     raise NotImplementedError()
+
 
 
 
