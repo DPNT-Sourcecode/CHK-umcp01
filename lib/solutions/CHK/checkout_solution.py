51a26bbb9ee95ffb099e7e2c109907f7bf8456d3
sbx_user1051 <sbx_user1051@169.254.68.201>
2022-05-28 11:25:08 +0000

    Sat May 28 11:25:08 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -13,12 +13,14 @@ def checkout(skus: str) -> int:
     if not isinstance(skus, str):
         return -1
 
-    code_counts = defaultdict()
+    code_counts = defaultdict(int)
 
     for code in skus:
         if not code in VALID_INPUT:
             return -1
+        code_counts[code] += 1
 
     raise NotImplementedError()
+
 
 
