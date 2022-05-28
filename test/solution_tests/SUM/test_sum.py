699e3d871ad8884f0b077524a91ce9f29e897f9d
sbx_user1051 <sbx_user1051@169.254.68.201>
2022-05-28 10:15:27 +0000

    Sat May 28 10:15:27 UTC 2022

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,6 +1,16 @@
+import pytest
 from solutions.SUM import sum_solution
 
 
 class TestSum:
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
+
+    def test_negative_input(self):
+        with pytest.raises(ValueError):
+            sum_solution.compute(-1, 2)
+
+    def test_input_greater_than_100(self):
+        with pytest.raises(ValueError):
+            sum_solution.compute(100, 2)
+
