c17c065c9ab7ba9827ac294a2f50ab579fcfa35b
sbx_user1051 <sbx_user1051@169.254.68.201>
2022-05-28 11:56:11 +0000

    Sat May 28 11:56:11 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -10,7 +10,7 @@ from typing import DefaultDict
 
 COSTS: dict = {
     "A": {"deals": [{"count": 5, "cost": 200}, {"count": 3, "cost": 130}], "cost": 50},
-    "B": {"deals": {"count": 2, "cost": 45}, "cost": 30},
+    "B": {"deals": [{"count": 2, "cost": 45}], "cost": 30},
     "C": {"cost": 20},
     "D": {"cost": 15},
 }
@@ -44,11 +44,12 @@ def checkout(skus: str) -> int:
             remainder = count
             for deal in cost_data["deals"]:
                 total_cost += (count // deal["count"]) * deal["cost"]
-                remainder = count % cost_data["deal"]["count"]
+                remainder = remainder % deal["count"]
             total_cost += remainder * cost_data["cost"]
         else:
             total_cost += count * cost_data["cost"]
 
     return total_cost
+
 
 
